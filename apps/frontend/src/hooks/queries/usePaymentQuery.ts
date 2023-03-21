b9b3a9027c5b46c977c9d2394e893bfdf7888248
Damian Kłos <alojzy231@gmail.com>
2023-03-22 00:14:56 +0100

    refactor: separated usePaymentQuery

---
+++ apps/frontend/src/hooks/queries/usePaymentQuery.ts
@@ -0,0 +1,39 @@
+import { useFirestoreQuery } from '@react-query-firebase/firestore';
+import firebase from 'firebase/compat';
+import { collection, query } from 'firebase/firestore';
+import { UseQueryResult } from 'react-query';
+
+import { firestore } from '@config/firebase';
+
+import FirestoreError = firebase.firestore.FirestoreError;
+
+type Payment = {
+  cost: number;
+};
+type QueryResult = { id: string } & Payment;
+type UsePaymentQueryResult = UseQueryResult<QueryResult[], FirestoreError>;
+export function usePaymentQuery(): UsePaymentQueryResult {
+  const ref = query(collection(firestore, 'spending')).withConverter<Payment>({
+    fromFirestore: (snapshot, options) => {
+      const data = snapshot.data(options);
+
+      // it is recommended way to return data from converter
+      return {
+        cost: data.cost,
+      };
+    },
+    toFirestore: (data: Payment) => data,
+  });
+
+  return useFirestoreQuery(['spending'], ref, undefined, {
+    select: (snapshot) =>
+      snapshot.docs.map((documentSnapshot) => {
+        const data = documentSnapshot.data();
+
+        return {
+          id: documentSnapshot.id,
+          ...data,
+        };
+      }),
+  });
+}
